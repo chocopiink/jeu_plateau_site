84b66bf4b0334021d61561686df4909b104e6234
Florine <florine.dobigny@gmail.com>
2024-05-09 17:36:32 +0200

    remise à zéro

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var menuMobile = document.getElementById("nav-menu");
+
+function toggleMenu() {
+    menuMobile.classList.toggle("menu-open");
+}
+
+function closeMenu() {
+    menuMobile.classList.remove("menu-open");
+}
